c00ee944e4d45641893aa5a3bc915ded59811676
ziginsider <ziginsider@gmail.com>
2017-12-02 00:25:13 +0300

    userDao complete

--- app/src/main/java/io/github/ziginsider/codelabpersistenceroom/db/UserDao.java
+++ app/src/main/java/io/github/ziginsider/codelabpersistenceroom/db/UserDao.java
@@ -1,9 +1,12 @@
 package io.github.ziginsider.codelabpersistenceroom.db;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+
+import static android.arch.persistence.room.OnConflictStrategy.IGNORE;
 
 import java.util.List;
 
@@ -23,9 +26,24 @@ public interface UserDao {
     @Query("select * from user where name = :firstName and lastName = :lastName")
     List<User> findUserByNameAndLastName(String firstName, String lastName);
 
-    @Insert(onConflict = Ignore)
+    @Insert(onConflict = IGNORE)
     void insertUser(User user);
 
+    @Delete
+    void deleteUser(User user);
 
-    @Query()
-    @Query() }
+    @Query("delete from user where name like :badName OR lastName like :badName")
+    void deleteUsesrsByName(String badName);
+
+    @Insert(onConflict = IGNORE)
+    void insertOrReplaceUsers(User... users);
+
+    @Delete
+    void deleteUsers(User user1, User user2);
+
+    @Query("SELECT * FROM User WHERE age < :age")
+    List<User> findUsersYangerThan(int age);
+
+    @Query("DELETE FROM User")
+    void deleteAll();
+}
