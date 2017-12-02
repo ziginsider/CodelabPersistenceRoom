157ebf16155efcf16c8ee9450b086ca8a649f887
ziginsider <ziginsider@gmail.com>
2017-12-02 20:16:54 +0300

    Loan Dao complete

--- app/src/main/java/io/github/ziginsider/codelabpersistenceroom/db/LoanDao.java
+++ app/src/main/java/io/github/ziginsider/codelabpersistenceroom/db/LoanDao.java
@@ -2,9 +2,11 @@ package io.github.ziginsider.codelabpersistenceroom.db;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.TypeConverters;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -22,4 +24,18 @@ public interface LoanDao {
             "INNER JOIN Book ON Loan.book_id = Book.id " +
             "INNER JOIN User ON User.id = Loan.user_id ")
     LiveData<List<LoanWithUserAndBook>> findAllWithUserAndBook();
+
+    @Query("SELECT Loan.id, Book.title AS title, User.name AS name, Loan.startTime, Loan.endTime " +
+            "FROM Book " +
+            "INNER JOIN Loan ON Loan.book_id = Book.id " +
+            "INNER JOIN User ON User.id = Loan.user_id " +
+            "WHERE User.name LIKE :userName " +
+            "AND Loan.endTime > :after ")
+    LiveData<List<LoanWithUserAndBook>> findLoansByNameAfter(String userName, Date after);
+
+    @Insert()
+    void insertLoan(Loan loan);
+
+    @Query("DELETE FROM Loan")
+    void deleteAll();
 }
