c37a9dc178020bdee329012e3ea3452673bab8c1
ziginsider <ziginsider@gmail.com>
2017-12-02 15:43:33 +0300

    Book dao complete

--- app/src/main/java/io/github/ziginsider/codelabpersistenceroom/db/BookDao.java
+++ app/src/main/java/io/github/ziginsider/codelabpersistenceroom/db/BookDao.java
@@ -2,9 +2,15 @@ package io.github.ziginsider.codelabpersistenceroom.db;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 
+import java.util.Date;
 import java.util.List;
+
+import static android.arch.persistence.room.OnConflictStrategy.IGNORE;
+import static android.arch.persistence.room.OnConflictStrategy.REPLACE;
 
 /**
  * Created by zigin on 02.12.2017.
@@ -18,8 +24,51 @@ public interface BookDao {
 
     @Query("SELECT * FROM Book " +
             "INNER JOIN Loan ON Loan.book_id = Book.id " +
-            "INNER JOIN User ON Loan.user_id = User.id " +
+            "INNER JOIN User ON User.id = Loan.user_id " +
             "WHERE User.name LIKE :userName")
     LiveData<List<Book>> findBooksBorrowedByName(String userName);
 
+    @Query("SELECT * FROM Book " +
+            "INNER JOIN Loan ON Loan.book_id = Book.id " +
+            "INNER JOIN User ON User.id = Loan.user_id " +
+            "WHERE User.name LIKE :userName " +
+            "AND Loan.endTime > :after ")
+    LiveData<List<Book>> findBooksBorrowedByNameAfter(String userName, Date after);
+
+    @Query("SELECT * FROM Book " +
+            "INNER JOIN Loan ON Loan.book_id = Book.id " +
+            "INNER JOIN User ON User.id = Loan.user_id " +
+            "WHERE User.name LIKE :userName")
+    List<Book> findBooksBorrowedByNameSync(String userName);
+
+    @Query("SELECT * FROM Book " +
+            "INNER JOIN Loan ON Loan.book_id LIKE Book.id " +
+            "WHERE Loan.user_id LIKE :userId ")
+    LiveData<List<Book>> findBooksBorrowedByUser(String userId);
+
+    @Query("SELECT * FROM Book " +
+            "INNER JOIN Loan ON Loan.book_id LIKE Book.id " +
+            "WHERE Loan.user_id LIKE :userId " +
+            "AND Loan.endTime > :after ")
+    LiveData<List<Book>> findBooksBorrowedByUserAfter(String userId, Date after);
+
+    @Query("SELECT * FROM Book " +
+            "INNER JOIN Loan ON Loan.book_id LIKE Book.id " +
+            "WHERE Loan.user_id LIKE :userId ")
+    List<Book> findBooksBorrowedByUserSync(String userId);
+
+    @Query("SELECT * FROM Book")
+    LiveData<List<Book>> findAllBooks();
+
+    @Query("SELECT * FROM Book")
+    List<Book> findAllBooksSync();
+
+    @Insert(onConflict = IGNORE)
+    void insertBook(Book book);
+
+    @Update(onConflict = REPLACE)
+    void updateBook(Book book);
+
+    @Query("DELETE FROM Book")
+    void deleteAll();
 }
